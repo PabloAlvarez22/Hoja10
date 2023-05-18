61bb4147679e1d6b811b300398202b569490c293
Michelle Mejía Villela <97720056+michellemej22596@users.noreply.github.com>
2023-05-17 23:26:44 -0600

    Diagrama UML

--- src/testing/test.java
+++ src/testing/test.java
@@ -21,4 +21,21 @@ public class test extends TestCase {
  
 	}
 	
+	 public void testAddEdgeAndHasEdge() {
+		 Graph graph = new Graph(3);
+			graph.addVertex("Buenos Aires");
+			graph.addVertex("Sao Paulo");
+			graph.addVertex("Lima");
+
+			graph.addEdge(0, 1, 10); // Buenos Aires a Sao Paulo toma 10 horas
+			graph.addEdge(1, 2, 12); // Sao Paulo a Lima  toma 12 horas
+
+	        assertTrue(graph.hasEdge(0, 1)); // Cuentan con conexion
+	        assertTrue(graph.hasEdge(1, 2)); // Cuentan con conexion
+	       
+	        assertFalse(graph.hasEdge(2, 3)); // No tienen conexion
+	        assertFalse(graph.hasEdge(0, 2)); // No tienen conexion
+	        assertFalse(graph.hasEdge(1, 3)); // No tienen conexion
+	    }
+	
 }
